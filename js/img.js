b5d78f81bd89d2eae84aa8eb6deab92a75209904
Pandastrange <110817408+Pandastrange@users.noreply.github.com>
2024-02-26 16:43:18 +0100

    Add files via upload

---
+++ js/img.js
@@ -0,0 +1,13 @@
+const videoContainer = document.getElementById('video-container');
+const imageContainer = document.getElementById('image-container');
+const images = imageContainer.getElementsByTagName('img');
+
+for (let i = 0; i < images.length; i++) {
+    images[i].addEventListener('click', function() {
+        const img = document.createElement('img');
+        img.src = this.src;
+        img.alt = this.alt;
+        videoContainer.innerHTML = '';
+        videoContainer.appendChild(img);
+    });
+}
